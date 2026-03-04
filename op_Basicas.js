963c64bb0eda8df7ecd9f2ffa36bc4002cdf926a
KevinCabrera04 <kevincabrera35@aragon.unam.mx>
2026-03-03 21:01:17 -0600

    Operaciones Básicas

---
+++ op_Basicas.js
@@ -0,0 +1,10 @@
+//Operaciones basicas
+var dato1=parseInt(prompt("Ingresa un numero 1"));
+var dato2=parseInt(prompt("Ingresar numero 2"));
+
+console.log("Operacionesc basicas\n" );
+console.log("Los Datos son=\n Dato 1=" +dato1+ "\n Dato 2="+dato2);
+
+console.log("Suma:"+dato1+"+"+dato2+"="+(dato1+dato2));
+console.log("Resta:"+dato1+"-"+dato2+"="+(dato1-dato2));
+console.log("Multiplicación:"+dato1+"*"+dato2+"="+(dato1*dato2));
